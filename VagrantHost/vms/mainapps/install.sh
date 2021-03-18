3675763d67fd0d1f7989dc79e174314e1b293b1d
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-03-18 05:24:18 +0200

    Update install.sh

--- VagrantHost/vms/mainapps/install.sh
+++ VagrantHost/vms/mainapps/install.sh
@@ -1,13 +1,11 @@
-     apt-get update
-     apt-get upgrade -y
-     apt-get install fail2ban -y
-     apt-get install zip -y
-     apt-get install curl -y
-     apt-get install wget -y
-     apt-get update
-     sudo usermod -aG sudo vagrant
-     echo "WHHOOOOOOOOOOOOAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHH"
-     sleep 5
+     sudo apt-get update
+     sudo apt-get upgrade -y
+     sudo apt-get install fail2ban -y
+     sudo apt-get install zip -y
+     sudo apt-get install curl -y
+     sudo apt-get install wget -y
+     sudo apt-get update
+     
      sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
      sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
      sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
@@ -15,25 +13,28 @@
      sudo apt install docker-ce -y
      sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
      sudo chmod +x /usr/local/bin/docker-compose
-     
      sudo apt-get update
-     cd /var
-     cd www
+     
+     cd /home
+     cd vagrant
+     cd docker
      cd odoo/addons
      sudo unzip ohrms_core-14.0.1.0.0.zip
      sudo unzip base_accounting_kit-14.0.2.6.5.zip
      sudo docker-compose up -d
-     sleep 20s
-     cd /var
-     cd www
+     
+     cd /home
+     cd vagrant
+     cd docker
      cd organizr
      sudo docker-compose up -d
-     sleep 20s
-     cd /var
-     cd www
+     
+     cd /home
+     cd vagrant
+     cd docker
      cd bookstack
      sudo docker-compose up -d
-     sleep 20s
+     
      cd /opt
      sudo mkdir noip && cd noip
      sudo wget https://github.com/clickonrefresh/Automate-Noip-DUC/archive/main.zip
