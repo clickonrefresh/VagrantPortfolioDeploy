cb5763b34b3455d2c191c3ae5c7449a2c165ab87
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-03-18 12:22:10 +0200

    Update install.sh

--- VagrantHost/vms/2ndapps/install.sh
+++ VagrantHost/vms/2ndapps/install.sh
@@ -1,39 +1,34 @@
-     sudo apt-get update
-     sudo apt-get upgrade -y
-     sudo apt-get install fail2ban -y
-     sudo apt-get install zip -y
-     sudo apt-get install wget -y
-     sudo apt-get install curl -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install fail2ban -y
+sudo apt-get install zip -y
+sudo apt-get install wget -y
+sudo apt-get install curl -y
      
-     sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-     sudo apt-get update
-     sudo apt install docker-ce -y
-     sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-     sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get update
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+     
       
-     cd /home
-     cd vagrant
-     cd docker
-     cd vscodeserver
-     sudo docker-compose up -d
+cd /home/ubuntu-focal/docker/
+cd vscodeserver
+sudo docker-compose up -d
+
+cd /home/ubuntu-focal/docker/
+cd homeassistant
+sudo docker-compose up -d
+
+cd /home/ubuntu-focal/docker/
+cd teleport
+sudo docker-compose up -d
      
-     cd /home
-     cd vagrant
-     cd docker
-     cd homeassistant
-     sudo docker-compose up -d
-     
-     cd /home
-     cd vagrant
-     cd docker
-     cd teleport
-     sudo docker-compose up -d
-     
-     
-     cd /opt
-     sudo mkdir noip && cd noip
-     sudo wget https://github.com/clickonrefresh/Automate-Noip-DUC/archive/main.zip
-     sudo unzip main.zip
-     sudo init 6
+cd /opt
+sudo mkdir noip && cd noip
+sudo wget https://github.com/clickonrefresh/Automate-Noip-DUC/archive/main.zip
+sudo unzip main.zip
+sudo apt update && sudo apt upgrade -y
+sudo init 6
