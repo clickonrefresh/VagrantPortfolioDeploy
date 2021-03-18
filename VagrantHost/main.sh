6033a9ccfe43c03feb883eb73a7f641ed46f5251
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-03-18 04:52:03 +0200

    Update main.sh

--- VagrantHost/main.sh
+++ VagrantHost/main.sh
@@ -3,29 +3,30 @@
 # Install Virtualbox, Vagrant, Vagrant Os.
 
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install zip -y
-sudo apt install curl -y
+#sudo apt update
+#sudo apt upgrade -y
+#sudo apt install zip -y
+#sudo apt install curl -y
 
-sudo apt install virtualbox -y
+#sudo apt install virtualbox -y
 
-sudo curl -O https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
-sudo apt install ./vagrant_2.2.14_x86_64.deb
+#sudo curl -O https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
+#sudo apt install ./vagrant_2.2.14_x86_64.deb
 vagrant --version
 
-vagrant plugin install vagrant-vbguest
-vagrant plugin update vagrant-vbguest
+#vagrant plugin install vagrant-vbguest
+#vagrant plugin update vagrant-vbguest
 
 cd vms/admin
 vagrant up
+vagrant ssh
 cd ..
 
 
-cd mainapps
-vagrant up
-cd ..
+#cd mainapps
+#vagrant up
+#cd ..
 
-cd 2ndapps
-vagrant up
-cd ..
+#cd 2ndapps
+#vagrant up
+#cd ..
