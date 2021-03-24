98685053062e1ab85ac7bce20ec70c76bb83b390
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-03-24 22:22:13 +0200

    Update main.sh

--- VagrantHost/main.sh
+++ VagrantHost/main.sh
@@ -8,7 +8,7 @@ sudo apt upgrade -y
 sudo apt install zip -y
 sudo apt install curl -y
 
-#sudo apt install virtualbox -y
+sudo apt install virtualbox -y
 
 sudo curl -O https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
 sudo apt install ./vagrant_2.2.14_x86_64.deb
@@ -23,10 +23,10 @@ vagrant ssh
 cd ..
 
 
-#cd mainapps
-#vagrant up
-#cd ..
+cd mainapps
+vagrant up
+cd ..
 
-#cd 2ndapps
-#vagrant up
-#cd ..
+cd 2ndapps
+vagrant up
+cd ..
